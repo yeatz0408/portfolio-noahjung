39a5445acd5a415d2d786c283ad1fbd4d5db9fe4
Noah Jung <yeatz0408@gmail.com>
2026-03-24 11:36:18 +0900

    Initial Commit

---
+++ portfolio-noahjung/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home: React.FC = () => {
+    return (
+        <>
+            Noah Jung<br/>
+            Fullstack Web Developer
+        </>
+    )
+}
+
+export default Home
